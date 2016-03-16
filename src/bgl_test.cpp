8a7ac3d1354f5a6d1ed45cf5caee352a9d3559ef
Aven Bross <kieroda@gmail.com>
2016-03-16 23:50:22 +0000

    First commit.

---
+++ src/bgl_test.cpp
@@ -0,0 +1,152 @@
+//=======================================================================
+// Copyright 2007 Aaron Windsor
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+#include <iostream>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <vector>
+
+#include <boost/graph/planar_canonical_ordering.hpp>
+#include <boost/graph/is_straight_line_drawing.hpp>
+#include <boost/graph/chrobak_payne_drawing.hpp>
+#include <boost/graph/boyer_myrvold_planar_test.hpp>
+#include <boost/graph/graphviz.hpp>
+
+#include "path_coloring.h"
+
+
+
+using namespace boost;
+
+//a class to hold the coordinates of the straight line embedding
+struct coord_t
+{
+  std::size_t x;
+  std::size_t y;
+};
+
+
+int main(int argc, char** argv)
+{
+  typedef adjacency_list
+    < vecS,
+      vecS,
+      undirectedS,
+      property<vertex_index_t, int>
+    > graph;
+
+  
+
+  //Define the storage type for the planar embedding
+  typedef std::vector< std::vector< graph_traits<graph>::edge_descriptor > > 
+    embedding_storage_t;
+  typedef boost::iterator_property_map
+    < embedding_storage_t::iterator, 
+      property_map<graph, vertex_index_t>::type 
+    >
+    embedding_t;
+
+
+
+  // Create the graph - a maximal planar graph on 7 vertices. The functions
+  // planar_canonical_ordering and chrobak_payne_straight_line_drawing both
+  // require a maximal planar graph. If you start with a graph that isn't
+  // maximal planar (or you're not sure), you can use the functions
+  // make_connected, make_biconnected_planar, and make_maximal planar in
+  // sequence to add a set of edges to any undirected planar graph to make
+  // it maximal planar.
+  
+  graph g(3);
+  add_edge(0,1,g);
+  add_edge(1,2,g);
+  add_edge(0,3,g);
+
+  // Create the planar embedding
+  embedding_storage_t embedding_storage(num_vertices(g));
+  embedding_t embedding(embedding_storage.begin(), get(vertex_index,g));
+
+  boyer_myrvold_planarity_test(boyer_myrvold_params::graph = g,
+                               boyer_myrvold_params::embedding = embedding);
+  
+  for(int i=0; i<3; i++)
+  {                      
+  	  std::cout << "Adjacency list for " << i << ".\n";       
+	  for(auto iter = embedding[i].begin(); iter != embedding[i].end(); iter++)
+	  {
+	  	std::cout << "{" << source(*iter, g) << ", " << target(*iter, g)
+	  		<< "} adj = " << getIncidentVertex(i,*iter,g) << "\n"; 
+	  }
+	  std::cout << "\n";
+  }
+
+  std::vector<graph::vertex_descriptor> p;
+  std::vector<graph::vertex_descriptor> q;
+  
+  p.push_back(0);
+  
+  q.push_back(1);
+  q.push_back(2);
+  
+  std::map<graph::vertex_descriptor,int> color_map;
+  boost::associative_property_map< std::map<graph::vertex_descriptor, int> >
+    color_property_map(color_map);
+  
+  int color = 0;
+  
+  poh_color_recursive(g, embedding, p.begin(), p.end(), q.begin(), q.end(), color_property_map, color);
+
+  // Find a canonical ordering
+  /*std::vector<graph_traits<graph>::vertex_descriptor> ordering;
+  planar_canonical_ordering(g, embedding, std::back_inserter(ordering));
+
+
+  //Set up a property map to hold the mapping from vertices to coord_t's
+  typedef std::vector< coord_t > straight_line_drawing_storage_t;
+  typedef boost::iterator_property_map
+    < straight_line_drawing_storage_t::iterator, 
+      property_map<graph, vertex_index_t>::type 
+    >
+    straight_line_drawing_t;
+
+  straight_line_drawing_storage_t straight_line_drawing_storage
+    (num_vertices(g));
+  straight_line_drawing_t straight_line_drawing
+    (straight_line_drawing_storage.begin(), 
+     get(vertex_index,g)
+     );
+
+
+
+  // Compute the straight line drawing
+  chrobak_payne_straight_line_drawing(g, 
+                                      embedding, 
+                                      ordering.begin(),
+                                      ordering.end(),
+                                      straight_line_drawing
+                                      );
+  
+
+
+  std::cout << "The straight line drawing is: " << std::endl;
+  graph_traits<graph>::vertex_iterator vi, vi_end;
+  for(boost::tie(vi,vi_end) = vertices(g); vi != vi_end; ++vi)
+    {
+      coord_t coord(get(straight_line_drawing,*vi));
+      std::cout << *vi << " -> (" << coord.x << ", " << coord.y << ")" 
+                << std::endl;
+    }
+
+  // Verify that the drawing is actually a plane drawing
+  if (is_straight_line_drawing(g, straight_line_drawing))
+    std::cout << "Is a plane drawing." << std::endl;
+  else
+    std::cout << "Is not a plane drawing." << std::endl;*/
+
+  return 0;
+}
---
+++ src/path_coloring.h
@@ -0,0 +1,242 @@
+/*
+ * path_coloring.h
+ * Author: Aven Bross
+ * 
+ * Implementation of several algorithms for path coloring graphs.
+ */
+
+#ifndef __PATH_COLORING_H
+#define __PATH_COLORING_H
+
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <stdexcept>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/property_map/property_map.hpp>
+
+namespace boost {
+
+	template<typename Graph>
+	typename graph_traits<Graph>::vertex_descriptor
+		getIncidentVertex(typename graph_traits<Graph>::vertex_descriptor vertex,
+		typename graph_traits<Graph>::edge_descriptor edge, const Graph & graph)
+	{
+		auto adj = source(edge, graph);
+		if(adj == vertex)
+			adj = target(edge, graph);
+		return adj;
+	}
+
+	/*
+	 * Preconditions: graph is a weakly triangulated planar graph the neighbors of each
+	 * vertex arranged in clockwise order for some embedding. p and q are paths with
+	 * p above q (in clockwise orientation scheme) such that joining the beginnings and
+	 * ends of the paths forms a chordless cycle, and with p and q each colored a
+	 * different color. new_color is a third color different from the color of p and q.
+	 * 
+	 * Postconditions: returns a copy of g with the face enclosed by the cycle pq
+	 * 3-colored such that each color class induces a disjoint union of paths.
+	 */
+	template<typename Graph, typename Embedding, typename Coloring, typename Color,
+		typename VertexIter>
+	void poh_color_recursive(const Graph & graph, const Embedding & embedding,
+		VertexIter p_0, VertexIter p_1, VertexIter q_0, VertexIter q_1, Coloring & coloring,
+		Color new_color)
+	{
+		typedef graph_traits<Graph> GraphTraits;
+		typedef typename GraphTraits::vertex_descriptor vertex_descriptor;
+		typedef typename GraphTraits::edge_descriptor edge_descriptor;
+		typedef typename property_traits<Coloring>::value_type ColorType;
+		
+		/*BOOST_CONCEPT_ASSERT(( GraphConcept<Graph> ));
+		BOOST_CONCEPT_ASSERT(( ReadWritePropertyMapConcept<Coloring, vertex_descriptor> ));
+		BOOST_CONCEPT_ASSERT(( IntegerConcept<ColorType> ));
+		BOOST_CONCEPT_ASSERT (( L
+		BOOST_STATIC_ASSERT(( is_same<typename VertexIter::value_type,
+			vertex_descriptor>::value ));*/
+		
+		// Base case: K_2
+		if(p_1 - p_0 == 1 && q_1 - q_0 == 1)
+		{
+			return;
+		}
+	
+		// Find inner triangulation vertex for beginning of paths
+		vertex_descriptor t_0 = *p_0;
+		
+		// Iterate incident edges in clockwise embedded order
+		for(auto iter = embedding[*p_0].begin(); iter != embedding[*p_0].end(); iter++)
+		{
+			// Locate vertex on the end of edge
+			vertex_descriptor neighbor = getIncidentVertex(*p_0, *iter, graph);
+			
+			if(neighbor == *q_0)	// Find begining vertex of second path
+			{
+				// Triangulation vertex is one back counterclockwise
+				if(iter == embedding[*p_0].begin())
+					t_0 = getIncidentVertex(*p_0, *(embedding[*p_0].end() - 1), graph);	
+				else
+					t_0 = getIncidentVertex(*p_0, *(iter - 1), graph);
+				break;
+			}
+		}
+	
+		if(t_0 == *p_0) throw std::runtime_error("no triangulation vertex found at start of paths.");
+	
+		// Case 1: First triangle hits p or q
+		if(p_1 - p_0 > 1 && t_0 == *(p_0 + 1))
+		{
+			// Removing triangle removes first vertex of p
+			poh_color_recursive(graph, embedding, p_0 + 1, p_1, q_0, q_1, coloring, new_color);
+			
+			return;
+		}
+		else if(q_1 - q_0 > 1  && t_0 == *(q_0 + 1))
+		{
+			// Removing triangle removes first vertex of q
+			poh_color_recursive(graph, embedding, p_0, p_1, q_0 + 1, q_1, coloring, new_color);
+		
+			return;
+		}
+	
+		// Find inner triangulation vertex for end of paths
+		vertex_descriptor t_1 = *q_1;
+	
+		// Iterate incident edges in clockwise embedded order
+		for(auto iter = embedding[*(q_1 - 1)].begin(); iter != embedding[*(q_1 - 1)].end(); iter++)
+		{
+			// Locate vertex on the end of edge
+			vertex_descriptor neighbor = getIncidentVertex(*(q_1 - 1), *iter, graph);
+			
+			if(neighbor == *(p_1 - 1))	// Find begining vertex of second path
+			{
+				// Triangulation vertex is one back counterclockwise
+				if(iter == embedding[*(q_1 - 1)].begin())
+					t_1 = getIncidentVertex(*p_0, *(embedding[*(q_1 - 1)].end() - 1), graph);	
+				else
+					t_1 = getIncidentVertex(*(q_1 - 1), *(iter - 1), graph);
+				break;
+			}
+		}
+	
+		if(t_1 == *q_1) throw std::runtime_error("no triangulation vertex found at end of paths.");
+	
+		// Case 2: Second triangle hits p or q
+		if(p_1 - p_0 > 1 && t_1 == *(p_1 - 2))
+		{	
+			// Removing triangle removes last vertex of p
+			poh_color_recursive(graph, embedding, p_0, p_1 - 1, q_0, q_1, coloring, new_color);
+		
+			return;
+		}
+		else if(q_1 - q_0 > 1  && t_1 == *(q_1 - 2))
+		{
+			// Removing triangle removes last vertex of q
+			poh_color_recursive(graph, embedding, p_0, p_1, q_0, q_1 - 1, coloring, new_color);
+		
+			return;
+		}
+		/*
+		// Case 3: Search for path-bridging edge
+		{
+			std::unordered_map<Label,VertexIter> p_map;
+		
+			// Mark all edges from vertices on p
+			for(auto p_iter = p_0; p_iter != p_1; p_iter++)
+			{
+				p_map[*p_iter] = p_iter;
+			}
+		
+			for (auto q_iter = q_0; q_iter != q_1; ++q_iter)
+			{
+				for (auto q_nbr : graph[*q_iter])
+				{
+					if(p_map.count(q_nbr) != 0)
+					{
+						// Check if our edge is one of the outside edges
+						if(q_iter == q_0 && p_map[q_nbr] == p_0)
+							continue;
+						if(q_iter == q_1 - 1 && p_map[q_nbr] == p_1 - 1)
+							continue;
+					
+						// Recurse on "left" and "right" halves
+						poh_color_recursive(graph, p_0, p_map[q_nbr] + 1, q_0,q_iter + 1,
+							coloring, new_color);
+						poh_color_recursive(graph, p_map[q_nbr], p_1, q_iter, q_1,
+							coloring, new_color);
+					
+						return;
+					}
+				}
+			}
+		}
+	
+		// Case 4: Find splitting path
+		{
+			std::unordered_map<Label,Label> parent_map;
+			std::queue<Label> bfs_queue;
+		
+			// Perform BFS from t_1 inside the cycle and locate t_1-t_0 path
+			bfs_queue.push(t_1);
+			parent_map[t_1] = t_1;
+	
+			for(auto p_iter = p_0; p_iter != p_1; p_iter++)
+			{
+				parent_map[*p_iter] = *p_iter;
+			}
+		
+			for(auto q_iter = q_0; q_iter != q_1; q_iter++)
+			{
+				parent_map[*q_iter] = *q_iter;
+			}
+	
+			Label curr_vertex;
+		
+			while(!bfs_queue.empty() && curr_vertex != t_0)
+			{
+				curr_vertex = bfs_queue.front();
+				bfs_queue.pop();
+		
+				for(const auto & neighbor : graph[curr_vertex])
+				{
+					if(parent_map.count(neighbor) == 0)
+					{
+						bfs_queue.push(neighbor);
+						parent_map[neighbor] = curr_vertex;
+					}
+				}
+			}
+		
+			// A t_1 - t_0 path should be guaranteed, throw if not found
+			if(curr_vertex != t_0)
+			{
+				throw std::runtime_error("No splitting path or bridging edge.");
+			}
+		
+			// Backtrack to construct t_0 - t_1 path and color using new_color
+			std::vector<Label> splitting_path;
+			splitting_path.push_back(curr_vertex);
+			coloring[curr_vertex] = new_color;
+		
+			while(parent_map[curr_vertex] != curr_vertex)
+			{
+				curr_vertex = parent_map[curr_vertex];
+				splitting_path.push_back(curr_vertex);
+				coloring[curr_vertex] = new_color;
+			}
+		
+			// Recurse on top and bottom halves, now coloring with correct colors
+			poh_color_recursive(graph, p_0, p_1, splitting_path.begin(),
+				splitting_path.end(), coloring, coloring.at(*q_0));
+			poh_color_recursive(graph, splitting_path.begin(), splitting_path.end(),
+				q_0, q_1, coloring, coloring.at(*p_0));
+		}*/
+	}
+}
+
+#endif
